6b4df48b1665ced2ce99319d53ff9aa91c207512
Andrew Gentile <agentile1990@gmail.com>
2020-05-13 08:03:30 -0500

    Parse Arguments for Path

---
+++ src/Argv/index.js
@@ -0,0 +1,16 @@
+const yargs = require('yargs');
+import { name, version } from '../../package.json';
+
+const argv = yargs
+    .usage(`${name} ${version}`)
+    .usage('Usage: env-complete [options]')
+    .string('path')
+    .alias('path', 'p')
+    .nargs('path', 1)
+    .describe('path', 'path to directory containing .env and template files')
+    .help('help')
+    .alias('help', 'h')
+    .version('version', '1.0.1')
+    .alias('version', 'v').argv;
+
+export default argv;
