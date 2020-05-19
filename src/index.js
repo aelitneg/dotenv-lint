4e8b76f15ca3364a3348f9cff953409147b1a5bf
Andrew Gentile <agentile1990@gmail.com>
2020-05-19 07:50:36 -0500

    Load Files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import path from 'path';
 import argv from './Argv';
 
+import FileLoader from './FileLoader';
+
 console.log('[env-complete] starting');
 
-if (argv.path) {
-    console.log('[env-complete]', 'path:', path.join(process.cwd(), argv.path));
-}
+console.log(
+    '[env-complete]',
+    'path:',
+    argv.path ? path.join(process.cwd(), argv.path) : process.cwd(),
+);
+
+const fileLoader = new FileLoader(argv.path ? argv.path : process.cwd());
+
+console.log('[env-complete] loading .env and .env.template');
+Promise.all([fileLoader.load('.env'), fileLoader.load('.env.template')])
+    .then(function (files) {
+        const [envFile, envTemplate] = files;
+        console.log('[env-complete] loading complete!');
+    })
+    .catch(function (error) {
+        console.error('[env-complete]', error);
+    });
