5c1e938a1c6a3665e79750fe9679e61bc26325e9
Andrew Gentile <andrewgentile@fedora.manson9210.local>
2020-05-29 23:48:00 -0500

    Use NODE_ENV to Set Mode in Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,28 @@ const webpack = require('webpack');
 const NodemonPlugin = require('nodemon-webpack-plugin');
 const path = require('path');
 
-module.exports = {
-    mode: 'production',
-    target: 'node',
-    entry: './src/index.js',
-    output: {
-        filename: 'envlint.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-        new webpack.BannerPlugin({
-            banner: '#!/usr/bin/env node',
-            raw: true,
-            entryOnly: true,
-        }),
-        new NodemonPlugin({
-            args: ['--path', '/test'],
-        }),
-    ],
-    stats: {
-        // Ignore warnings due to yarg's dynamic module loading
-        warningsFilter: [/node_modules\/yargs/],
-    },
+module.exports = (env) => {
+    return {
+        mode: env.NODE_ENV === 'production' ? 'production' : 'development',
+        target: 'node',
+        entry: './src/index.js',
+        output: {
+            filename: 'envlint.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        plugins: [
+            new webpack.BannerPlugin({
+                banner: '#!/usr/bin/env node',
+                raw: true,
+                entryOnly: true,
+            }),
+            new NodemonPlugin({
+                args: ['--path', '/test'],
+            }),
+        ],
+        stats: {
+            // Ignore warnings due to yarg's dynamic module loading
+            warningsFilter: [/node_modules\/yargs/],
+        },
+    };
 };
